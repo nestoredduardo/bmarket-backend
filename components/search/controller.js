5f0cd4995376f1786250324390a38e3440e61ea7
NedduJS <nmamanipantoja@gmail.com>
2021-08-20 21:43:00 -0500

    Filter with JS

--- components/search/controller.js
+++ components/search/controller.js
@@ -1,13 +1,20 @@
 const db = require('../../db');
 
 const getResults = async (input) => {
-  const products = await db.query(
-    'SELECT * FROM product WHERE name LIKE %?%',
-    [input]
-  );
+  let filtered = [];
+  const productList = await db.query(`SELECT * FROM product`);
+
+  productList.forEach((item) => {
+    if (
+      item.name.toLowerCase().charAt(0) === input.toLowerCase().charAt(0) &&
+      item.name.localeCompare(input) !== -1
+    ) {
+      filtered.push(item);
+    }
+  });
 
   return {
-    products,
+    products: filtered,
   };
 };
 
