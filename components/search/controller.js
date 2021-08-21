091c4efa01b8f2d553c7a0d816faf68ff95058c2
Nestor Eduardo Mamani Pantoja <nmamanipantoja@gmail.com>
2021-08-20 21:16:59 -0500

    Update controller.js

--- components/search/controller.js
+++ components/search/controller.js
@@ -2,7 +2,7 @@ const db = require('../../db');
 
 const getResults = async (input) => {
   const products = await db.query(
-    'SELECT * FROM product WHERE CHARINDEX( ? , name) > 0',
+    'SELECT * FROM product WHERE name LIKE %?%',
     [input]
   );
 
