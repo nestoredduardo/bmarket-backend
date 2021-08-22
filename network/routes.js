ef794037bf07fdcb375b72cce5089a45193520a1
NedduJS <nmamanipantoja@gmail.com>
2021-08-22 09:29:50 -0500

    Base Url

--- network/routes.js
+++ network/routes.js
@@ -1,3 +1,4 @@
+const base = require('../components/base/network');
 const products = require('../components/products/network');
 const categories = require('../components/categories/network');
 const search = require('../components/search/network');
@@ -6,6 +7,7 @@ const routes = (server) => {
   server.use('/products', products);
   server.use('/categories', categories);
   server.use('/search', search);
+  server.use('/', base);
 };
 
 module.exports = routes;
